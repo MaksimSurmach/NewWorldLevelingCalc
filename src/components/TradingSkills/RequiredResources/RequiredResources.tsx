35c3506a8873cff2be331a9339971c4aed232e6a
Maksim Surmach <maxsurm@gmail.com>
2021-11-17 19:50:22 +0300

    Revert "Revert "some cool stuff""
    This reverts commit c45107cc99dd400194dadabc8677179213116885.

--- src/components/TradingSkills/RequiredResources/RequiredResources.tsx
+++ src/components/TradingSkills/RequiredResources/RequiredResources.tsx
@@ -21,8 +21,11 @@ import { Box } from '@mui/material';
 import RecepieTree from './RecepieTree';
 import ListItem from '@mui/material/ListItem';
 import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 
-import './reqres.scss'
+import './reqres.css'
+import IconRarity from '../IconRarity/IconRarity';
 
 const defaultResourceItem ={
   "type": "item",
@@ -93,7 +96,7 @@ export function RequiredResources() {
         {item.subIngredients.map((subitem:any, index:number)=>{
          
           return(
-            <MenuItem  key={subitem} value={subitem} >
+            <MenuItem  key={subitem} value={subitem} className="chooser">
                 <List >
                   <ListItem>
                     <ListItemAvatar>
@@ -134,11 +137,24 @@ export function RequiredResources() {
   });
   return(
     
-    <div>
-      <Avatar  src={process.env.PUBLIC_URL + "/images/CraftedItemIcon/" +  selectedRecipe.output.icon + ".png"} variant="square" />
- 
-      {selectedRecipe.name}
-    {resource}
+    <div className="center">
+      <div className="itemname">
+      <Grid container alignItems="left">
+          <Grid item xs>
+            <Typography gutterBottom variant="h4" component="div">
+            {/* <IconRarity url={process.env.PUBLIC_URL + "/images/CraftedItemIcon/" +  selectedRecipe.output.icon + ".png"} rarity={selectedRecipe.output.rarity}/> */}
+            <IconRarity width="60" src={process.env.PUBLIC_URL + "/images/CraftedItemIcon/" +  selectedRecipe.output.icon + ".png"} rarity={selectedRecipe.output.rarity} />
+            {/* <Avatar  src={process.env.PUBLIC_URL + "/images/CraftedItemIcon/" +  selectedRecipe.output.icon + ".png"} variant="square" sx={{ width: 60, height: 60 }} /> */}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography gutterBottom variant="h4" component="div">
+            {selectedRecipe.name}
+            </Typography>
+          </Grid>
+        </Grid>
+      </div>
+      {resource}
     </div>
   );
 }
